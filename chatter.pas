8c48fbc46ea4b645efaee9193c8b57394f53c31f
Jonas Hübotter <me@jonhue.me>
2018-02-27 15:49:03 +0100

    rename executable to

---
+++ chatter.pas
@@ -0,0 +1,22 @@
+program chatter;
+
+{$mode objfpc}{$H+}
+
+uses
+  {$IFDEF UNIX}{$IFDEF UseCThreads}
+  cthreads,
+  {$ENDIF}{$ENDIF}
+  Interfaces, // this includes the LCL widgetset
+  Forms, Form
+  { you can add units after this };
+
+{$R *.res}
+
+begin
+  Application.Title:='Chatter';
+  RequireDerivedFormResource := True;
+  Application.Initialize;
+  Application.CreateForm(TForm1, Form1);
+  Application.Run;
+end.
+
